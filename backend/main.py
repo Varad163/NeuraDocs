23dc0e9045887cc04825ba084b0935e7a6743f47
Varad163 <varadgholap163@gmail.com>
2025-11-30 15:14:48 +0530

    interpreter issue

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,8 @@ from backend.services.chunker import chunk_text
 from backend.services.pdf_reader import extract_text_from_pdf
 from backend.services.pinecone_rag import store_chunks, query_chunks
 from openai import OpenAI
+from fastapi.middleware.cors import CORSMiddleware
+
 import os
 from dotenv import load_dotenv
 
@@ -12,6 +14,15 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 app = FastAPI()
 
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],            # allow all for development
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.post("/extract")
 async def extract_endpoint(file: UploadFile = File(...)):
     contents = await file.read()
